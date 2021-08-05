6d5697f1eebed230d07e8fa4a8fa3e1890745d8e
Isaac <evenevernameistaken@gmail.com>
2021-08-05 12:21:02 +1000

    Commented my code

--- TheFabricOfSpace/Assets/Scripts/Block.cs
+++ TheFabricOfSpace/Assets/Scripts/Block.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    // Shows whether a direction is traverable
     public bool[] traversable = new bool[4];
 
+    // Triggers that determine whether a player can jump in a direction
     public BoxCollider[] jumpTriggers = new BoxCollider[4];
 
+    // End positions of the jumps
     public Vector3[] jumpLandings = new Vector3[4];
 
+    // Colliders that incase each block and prevent passage
     public BoxCollider[] colliders = new BoxCollider[5];
 
 
-    // Start is called before the first frame update
+    
     void Awake()
     {
+        // Sets the Jump triggers and colliders
         jumpTriggers = transform.GetChild(0).GetComponents<BoxCollider>();
         colliders = GetComponents<BoxCollider>();
 
+        // Loop through each direction and check, if the jump triggers and/or the colliders should be active
         for (int i = 0; i < traversable.Length; i++)
         {
 
@@ -33,6 +39,7 @@ public class Block : MonoBehaviour
             {
                 if (hit.distance > 1)
                 {
+                    // Hit distance is greater than 1 so the player can jump
                     jumpTriggers[i].enabled = true;
                     jumpLandings[i] = hit.transform.position + transform.up;
                     traversable[i] = false;
@@ -40,12 +47,14 @@ public class Block : MonoBehaviour
                 }
                 else if (hit.transform.gameObject.tag == "Block" || hit.transform.gameObject.tag == "Slope Upper")
                 {
+                    // Hit a block adjacent to current block so disable colliders
                     traversable[i] = true;
                     colliders[i].enabled = false;
                     jumpTriggers[i].enabled = false;
                 }
                 else
                 {
+                    // Hit something adjacent other than a block, enable colliders
                     traversable[i] = false;
                     colliders[i].enabled = true;
                     jumpTriggers[i].enabled = false;
@@ -53,6 +62,7 @@ public class Block : MonoBehaviour
             }
             else
             {
+                // Hit nothing, enable colliders
                 jumpTriggers[i].enabled = false;
                 traversable[i] = false;
                 colliders[i].enabled = true;
@@ -60,7 +70,8 @@ public class Block : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
+    // BockUpdate is called when conditions near a block changes, for example slab sheep activation
+    // Checks all the things that Awake checks
     public void BlockUpdate()
     {
         for (int i = 0; i < traversable.Length; i++)
@@ -85,7 +96,7 @@ public class Block : MonoBehaviour
                     traversable[i] = false;
                     colliders[i].enabled = true;
                 }
-                else //if (hit.transform.gameObject.tag == "Block" || hit.transform.gameObject.tag == "Slope Upper")
+                else
                 {
                     traversable[i] = true;
                     colliders[i].enabled = false;
