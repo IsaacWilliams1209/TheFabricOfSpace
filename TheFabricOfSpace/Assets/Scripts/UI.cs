dac82a074101820c63721e54918aeee9d5c30408
Michael <mrprice04@gmail.ocm>
2021-08-05 14:17:36 +1000

    UI Implementation V01
    Have the base canvas layouts set up, now going to add functionality to it

---
+++ TheFabricOfSpace/Assets/Scripts/UI.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    [SerializeField]
+    Image lavishTides;
+    [SerializeField]
+    Image gustyCliffs;
+    [SerializeField]
+    Image frostyFields;
+    [SerializeField]
+    private string plan01Name;
+    [SerializeField]
+    private string plan02Name;
+    [SerializeField]
+    private string plan03Name;
+
+    private GameObject lvlNameDisplay;
+
+    private void Start()
+    {
+        lavishTides.name = plan01Name;
+        gustyCliffs.name = plan02Name;
+        frostyFields.name = plan03Name;
+
+        lvlNameDisplay = transform.GetChild(7).gameObject;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            lvlNameDisplay.GetComponent<Text>().text = plan01Name;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            lvlNameDisplay.GetComponent<Text>().text = plan02Name;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            lvlNameDisplay.GetComponent<Text>().text = plan03Name;
+        }
+    }
+}
