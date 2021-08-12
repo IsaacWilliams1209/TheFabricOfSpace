dfd7d5000d2c1a94ca62ecf525db9e2cd3c31529
Michael Price <mrprice04@gmail.com>
2021-08-12 17:16:10 +1000

    UI WIP V2
    End of Day Push

--- TheFabricOfSpace/Assets/Scripts/UI_Manager_v2.cs
+++ TheFabricOfSpace/Assets/Scripts/UI_Manager_v2.cs
@@ -6,17 +6,16 @@ using UnityEngine.EventSystems;
 
 public class UI_Manager_v2 : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject button01, button02, button03, button04;
 
+    [SerializeField]
     List<GameObject> UiElements = new List<GameObject>();
 
     private int currIndex = 0;
 
     private void Start()
     {
-        ListSetUp();
-        EventSystem.current.firstSelectedGameObject = button01;
+
+        //EventSystem.current.firstSelectedGameObject = button01;
     }
 
     private void Update()
@@ -24,12 +23,5 @@ public class UI_Manager_v2 : MonoBehaviour
         
     }
 
-    private void ListSetUp()
-    {
-        UiElements.Add(button01);
-        UiElements.Add(button02);
-        UiElements.Add(button03);
-        UiElements.Add(button04);
-    }
 
 }
