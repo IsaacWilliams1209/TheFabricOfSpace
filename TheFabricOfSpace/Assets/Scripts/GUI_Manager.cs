dfb3c1fa7cd94c6c663dee710f23483ca79fe08b
Michael Price <mrprice04@gmail.com>
2021-09-15 14:36:34 +1000

    GUI First Pass Done
    Framework is set up for alpha

--- TheFabricOfSpace/Assets/Scripts/GUI_Manager.cs
+++ TheFabricOfSpace/Assets/Scripts/GUI_Manager.cs
@@ -4,30 +4,39 @@ using UnityEngine;
 
 public class GUI_Manager : MonoBehaviour
 {
-    GameObject currSheepType;
+    Shepherd currSheepType;
     bool isVoxelSheep;
     Vector3 originalTransform;
     GameObject defaultSheepIcon;
     GameObject voxelSheepIcon;
     Vector3 uiScaleSize = new Vector3(1.4f, 1.4f, 1.4f);
     float uiEffectSpeed = 0.8f;
+    PopUp_Manager popUpManager;
+    Camera camera;
 
     private void Start()
     {
-        currSheepType = GameObject.Find("Shepherd");
+        currSheepType = GameObject.Find("Shepherd").GetComponent<Shepherd>();
         defaultSheepIcon = GameObject.Find("Sheep_Icon01");
         voxelSheepIcon = GameObject.Find("Sheep_Icon02");
+        popUpManager = PopUp_Manager.GetInstance();
+        camera = Camera.main;
     }
 
     void Update()
     {
         IconUpdater();
-        Debug.Log(GameObject.Find("Shepherd").GetComponentInChildren<Sheep>().voxel);
+        PopUpManager();
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Debug.Log(currSheepType.GetComponentInChildren<Sheep>().gameObject.transform.position);
+
+        }
     }
 
     void IconUpdater()
     {
-        if (currSheepType.GetComponent<Shepherd>().activeSheep.GetComponent<Sheep>().voxel)
+        if (currSheepType.activeSheep.GetComponent<Sheep>().voxel)
         {
             if (!LeanTween.isTweening(voxelSheepIcon))
             {
@@ -44,6 +53,31 @@ public class GUI_Manager : MonoBehaviour
             }
         }
 
+    }
+
+    void PopUpManager()
+    {
+        if (currSheepType.activeSheep.GetComponent<Sheep>().canJump)
+        {
+            Vector3 tempPos = currSheepType.activeSheep.GetComponent<Sheep>().gameObject.transform.position;
+            popUpManager.popUps[0].gameObject.transform.position = new Vector3(tempPos.x, tempPos.y + 2, tempPos.z);
+            popUpManager.popUps[0].gameObject.transform.LookAt(popUpManager.popUps[0].gameObject.transform.position + camera.transform.rotation * Vector3.forward,
+                camera.transform.rotation * Vector3.up);
+            popUpManager.SwitchPopUp(PopUpType.RButton);
+        }
+        else if(currSheepType.activeSheep.GetComponent<Sheep>().canWake)
+        {
+            Vector3 tempPos = currSheepType.activeSheep.GetComponent<Sheep>().gameObject.transform.position;
+            popUpManager.popUps[1].gameObject.transform.position = new Vector3(tempPos.x, tempPos.y + 2, tempPos.z);
+            popUpManager.popUps[1].gameObject.transform.LookAt(popUpManager.popUps[1].gameObject.transform.position + camera.transform.rotation * Vector3.forward,
+                camera.transform.rotation * Vector3.up);
+            popUpManager.SwitchPopUp(PopUpType.SpaceBar);
+        }
+        else
+        {
+            popUpManager.ClosePopUps();
+
+        }
     }
 
     private void PulseEffect(GameObject currIcon)
