c5ce0acdfe04ab22f3892cfa4282299132700644
Michael Price <mrprice04@gmail.com>
2021-10-01 17:14:39 +1000

    Static Sheep

---
+++ TheFabricOfSpace/Assets/Scripts/StaticSheep.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticSheep : Sheep
+{
+    /// <summary> Works by attaching sheep to one side when pressing button and than follows the sheep around until deattached.
+    /// </summary>
+    /// <param name="sheep"></param>
+    public void ActivePowerUp(Sheep sheep)
+    {
+
+    }
+
+}
