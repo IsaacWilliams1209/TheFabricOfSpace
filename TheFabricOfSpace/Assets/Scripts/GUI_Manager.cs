452a5bc8df5d008af7711e22b7bce0d7e911486e
Michael Price <mrprice04@gmail.com>
2021-11-07 18:10:08 +1100

    GUI WIP v02

--- TheFabricOfSpace/Assets/Scripts/GUI_Manager.cs
+++ TheFabricOfSpace/Assets/Scripts/GUI_Manager.cs
@@ -1,58 +1,91 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GUI_Manager : MonoBehaviour
 {
     Shepherd currSheepType;
-    bool isVoxelSheep;
     Vector3 originalTransform;
-    GameObject defaultSheepIcon;
-    GameObject voxelSheepIcon;
+    GameObject defaultSheepIcon, slabSheepIcon, snowballSheepIcon, shockSheepIcon, sheepAsleepIcon;
+    GameObject activeSheepIcon, lastActiveIcon;
     Vector3 uiScaleSize = new Vector3(2.4f, 2.4f, 2.4f);
     float uiEffectSpeed = 0.8f;
     PopUp_Manager popUpManager;
     Camera camera;
 
+    Player currPlanetFace;
+
     private void Start()
     {
         currSheepType = GameObject.Find("Shepherd").GetComponent<Shepherd>();
-        defaultSheepIcon = GameObject.Find("Sheep_Icon01");
-        voxelSheepIcon = GameObject.Find("Sheep_Icon02");
+        defaultSheepIcon = GameObject.Find("DefaultSheepIcon");
+        slabSheepIcon = GameObject.Find("SlabSheepIcon");
+        snowballSheepIcon = GameObject.Find("SnowballSheepIcon");
+        shockSheepIcon = GameObject.Find("ShockSheepIcon");
+        sheepAsleepIcon = GameObject.Find("SheepAsleepIcon");
         popUpManager = PopUp_Manager.GetInstance();
         camera = Camera.main;
+        InitialGUILayOut();
     }
 
     void Update()
     {
-        IconUpdater();
+        //IconUpdater();
         PopUpManager();
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            Debug.Log(currSheepType.GetComponentInChildren<Sheep>().gameObject.transform.position);
-
-        }
     }
 
     void IconUpdater()
     {
-        if (currSheepType.activeSheep.GetComponent<Sheep>().sheepType == SheepType.Slab)
-        {
-            if (!LeanTween.isTweening(voxelSheepIcon))
+        if (currSheepType.activeSheep.GetComponent<Sheep>().sheepType == SheepType.Slab) {
+            if (!LeanTween.isTweening(slabSheepIcon))
             {
-                PulseEffect(voxelSheepIcon);
-                CancelTween(defaultSheepIcon);
+                activeSheepIcon = slabSheepIcon;
+                PulseEffect(slabSheepIcon);
+                CancelTween(lastActiveIcon);
             }
         }
-        else
-        {
-            if (!LeanTween.isTweening(defaultSheepIcon))
+        else if (currSheepType.activeSheep.GetComponent<Sheep>().sheepType == SheepType.Snowball) {
+            if (!LeanTween.isTweening(snowballSheepIcon))
             {
+                activeSheepIcon = snowballSheepIcon;
+                PulseEffect(snowballSheepIcon);
+                CancelTween(lastActiveIcon);
+            }
+        }
+        else if (currSheepType.activeSheep.GetComponent<Sheep>().sheepType == SheepType.Static) {
+            if (!LeanTween.isTweening(shockSheepIcon))
+            {
+                activeSheepIcon = shockSheepIcon;
+                PulseEffect(shockSheepIcon);
+                CancelTween(lastActiveIcon);
+            }
+        }
+        else {
+            if (!LeanTween.isTweening(defaultSheepIcon)) {
+                activeSheepIcon = defaultSheepIcon;
                 PulseEffect(defaultSheepIcon);
-                CancelTween(voxelSheepIcon);
+                CancelTween(lastActiveIcon);
             }
         }
 
+    }
+
+    private void SwitchGUILayout()
+    {
+        if (currPlanetFace.sidesCompleted == 1)
+        {
+
+        }
+    }
+
+    private void InitialGUILayOut()
+    {
+        activeSheepIcon = defaultSheepIcon;
+        GameObject temp = Instantiate(gameObject.transform.GetChild(0).gameObject, new Vector3(757, 241, 0), new Quaternion(0, 0, 0, 0));
+        int sheepCount = 1;
+        temp.name = "Asleep Sheep " + sheepCount;
+        temp.transform.parent = gameObject.transform;
     }
 
     void PopUpManager()
@@ -91,4 +124,6 @@ public class GUI_Manager : MonoBehaviour
         lastIcon.gameObject.transform.localScale = originalTransform;
         LeanTween.cancel(lastIcon);
     }
+
+
 }
