479d2d175ad9bcb32a8336295e8226d826125927
Isaac <evenevernameistaken@gmail.com>
2021-11-18 15:05:24 +1100

    Peepeepoopoo

---
+++ TheFabricOfSpace/Assets/Scripts/particles.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particles : MonoBehaviour
+{
+
+    public GameObject camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = -camera.transform.position;
+        transform.rotation = Quaternion.Inverse(camera.transform.rotation);
+    }
+}
